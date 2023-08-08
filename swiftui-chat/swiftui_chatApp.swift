c51a41b13c0a31e6d1cf9ef6cfecf07f3727aa22
Kairat Mynbayev <kairat09@gmail.com>
2023-08-08 11:44:48 +0600

    Initial Commit

---
+++ swiftui-chat/swiftui_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_chatApp.swift
+//  swiftui-chat
+//
+//  Created by Kairat Mynbayev on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
